521ad66c0698fcebce3a27753e1174d101063a56
Joseph Tamas <tamasjosephdaniel@gmail.com>
2021-11-03 17:13:03 +0200

    Created project

---
+++ SwiftUIMemorize/SwiftUIMemorize/SwiftUIMemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMemorizeApp.swift
+//  SwiftUIMemorize
+//
+//  Created by Joseph on 03.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
